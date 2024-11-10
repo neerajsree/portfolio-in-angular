959ff19601628804bda59d97494ba1f4748958fe
Neeraj <neerajsree008@gmail.com>
2024-11-10 21:59:37 +0530

    want to completegoals page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { GoalsComponent } from './goals/goals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,GoalsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
