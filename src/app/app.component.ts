51a3771d87cd4e1bd1868cb0eac035811ea7390f
Neeraj <neerajsree008@gmail.com>
2024-12-20 13:51:17 +0530

    want to complete progress bar in skill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { GoalsComponent } from './goals/goals.component';
+import {SkillComponent} from './skill/skill.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavComponent,GoalsComponent],
+  imports: [RouterOutlet,NavComponent,GoalsComponent,SkillComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
